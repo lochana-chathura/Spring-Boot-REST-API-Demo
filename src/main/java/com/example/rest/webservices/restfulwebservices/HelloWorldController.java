2260490b20eafafd49616be56f3200fe902e54c3
lochana-chathura <chathux.lc@gmail.com>
2018-08-08 14:44:57 +0530

    First Commit

---
+++ src/main/java/com/example/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.example.rest.webservices.restfulwebservices;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping(path = "hello-world")
+    public String helloworld(){
+        return "Hello World!";
+    }
+
+    @GetMapping(path = "hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello World, %s",name));
+    }
+}
